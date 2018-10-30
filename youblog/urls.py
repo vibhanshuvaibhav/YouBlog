0174e490500e25a56df1b1ef4f4e85e86a10fafc
hp <ayush.vibhanshu@gmail.com>
2018-10-30 15:54:58 +0530

    added comment support and security features

--- youblog/urls.py
+++ youblog/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include, url
+
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('youblogapp.urls')),
+	path('admin/', admin.site.urls),
+	path('', include('youblogapp.urls')),
+	path('accounts/', include('django.contrib.auth.urls')),
 ]
